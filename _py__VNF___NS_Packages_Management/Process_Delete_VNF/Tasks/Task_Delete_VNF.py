d7e6fd6577592ba7e1ff957a923177cdc1100442
Sergio Lebon <sle@ubiqube.com>
2022-07-12 16:17:04 +0200

    Use the SOL005 version for VNF Package management WF processes.

--- _py__VNF___NS_Packages_Management/Process_Delete_VNF/Tasks/Task_Delete_VNF.py
+++ _py__VNF___NS_Packages_Management/Process_Delete_VNF/Tasks/Task_Delete_VNF.py
@@ -1,3 +1,4 @@
+import json
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
@@ -10,7 +11,9 @@ if __name__ == "__main__":
     dev_var.add('vnf_package_id', var_type='String')
     context = Variables.task_call(dev_var)
     
-    vnfPkgApi = VnfPkgSol005(context["mano_ip"], context["mano_port"])
+    #Get SOL00X version from context.
+    sol_version = context.get('sol005_version')
+    vnfPkgApi = VnfPkgSol005(context["mano_ip"], context["mano_port"], sol_version)
     
     auth_mode = context['auth_mode']
     if auth_mode == 'oauth2' or auth_mode == 'oauth_v2':
@@ -19,6 +22,7 @@ if __name__ == "__main__":
         vnfPkgApi.set_parameters(auth_mode, context['mano_user'], context['mano_pass'])
 
     r = vnfPkgApi.vnf_packages_vnfpkgid_delete(context['vnf_package_id'])
-    
-    ret = MSA_API.process_content(vnfPkgApi.state, f'{r}', context, True)
+   
+    r_details = r.json().get('detail')
+    ret = MSA_API.process_content(vnfPkgApi.state, f'{r}' + ': ' + r_details, context, True) 
     print(ret)

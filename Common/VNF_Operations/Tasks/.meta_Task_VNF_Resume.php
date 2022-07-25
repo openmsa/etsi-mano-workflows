e016c975e8a9dcae89b6570e1b347c74a3624839
root <antoine.brun@ubiqube.com>
2022-07-25 12:21:41 +0000

    format PHP

--- Common/VNF_Operations/Tasks/.meta_Task_VNF_Resume.php
+++ Common/VNF_Operations/Tasks/.meta_Task_VNF_Resume.php
@@ -2,41 +2,34 @@
 <metadata>
     <map>
         <entry>
-            <key>MODEL</key>
+            <key>DISPLAYNAME</key>
+            <value>Task_VNF_Resume.php</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1658751417263</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1658751417221</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
         </entry>
         <entry>
             <key>FILE_TYPE</key>
             <value>text</value>
         </entry>
         <entry>
-            <key>DATE_MODIFICATION</key>
-            <value>1562252888882</value>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
         </entry>
         <entry>
             <key>COMMENT</key>
-            <value></value>
-        </entry>
-        <entry>
-            <key>REPOSITORY</key>
-        </entry>
-        <entry>
-            <key>DATE_CREATION</key>
-            <value>1562249273666</value>
-        </entry>
-        <entry>
-            <key>MANUFACTURER</key>
-        </entry>
-        <entry>
-            <key>CONFIGURATION_FILTER</key>
-            <value></value>
-        </entry>
-        <entry>
-            <key>TAG</key>
-            <value></value>
-        </entry>
-        <entry>
-            <key>TYPE</key>
-            <value>UPLOAD</value>
         </entry>
     </map>
 </metadata>
--- Common/VNF_Operations/Tasks/Task_VNF_Resume.php
+++ Common/VNF_Operations/Tasks/Task_VNF_Resume.php
@@ -13,27 +13,24 @@ $device_id = substr($context['deviceid'], 3);
 $index = 0;
 foreach ($context['servers'] as $server) {
 
-$server_id = $server['instance_id'];
+    $server_id = $server['instance_id'];
 
-$response = _nova_server_action ($device_id, $server_id, "Resume");
-$response = json_decode($response, true);
-if ($response['wo_status'] !== ENDED) {
-	$response = json_encode($response);
-	echo $response;
-	exit;
-}
+    $response = _nova_server_action($device_id, $server_id, "Resume");
+    $response = json_decode($response, true);
+    if ($response['wo_status'] !== ENDED) {
+        $response = json_encode($response);
+        echo $response;
+        exit;
+    }
 
-$response = wait_for_server_status($device_id, $server_id, ACTIVE, $context);
-$response = json_decode($response, true);
-if ($response['wo_status'] !== ENDED) {
-	$response = json_encode($response);
-	echo $response;
-	exit;
-}
-
+    $response = wait_for_server_status($device_id, $server_id, ACTIVE, $context);
+    $response = json_decode($response, true);
+    if ($response['wo_status'] !== ENDED) {
+        $response = json_encode($response);
+        echo $response;
+        exit;
+    }
 }
 
 $ret = prepare_json_response(ENDED, "Servers Resumed Successfully.", $context, true);
 echo $ret;
-
-?>

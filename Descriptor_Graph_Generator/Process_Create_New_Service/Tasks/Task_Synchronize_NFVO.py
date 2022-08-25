e310f7f0c4927c6e1068ba6275e114642304f00c
Sergio Lebon <sle@ubiqube.com>
2022-08-25 11:40:44 +0200

    Add Sync NFVO MS process task during the new service creation.

---
+++ Descriptor_Graph_Generator/Process_Create_New_Service/Tasks/Task_Synchronize_NFVO.py
@@ -0,0 +1,20 @@
+import time
+from msa_sdk.variables import Variables
+from msa_sdk.msa_api import MSA_API
+from msa_sdk.order import Order
+
+if __name__ == "__main__":
+
+    dev_var = Variables()
+    context = Variables.task_call(dev_var)
+
+    device_short_id = context["nfvo_me"][3:]
+    
+    time.sleep(6)
+    order = Order(str(device_short_id))
+    order.command_synchronize(timeout=60)
+
+    ret = MSA_API.process_content('ENDED',
+        f'Device {context["nfvo_me"]} synchronized', context, True)
+
+    print(ret)

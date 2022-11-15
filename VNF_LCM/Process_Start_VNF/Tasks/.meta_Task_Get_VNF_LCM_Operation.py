a86bbf44b981a10720a764ad98530b814af3da49
Sergio Lebon <sle@ubiqube.com>
2022-11-15 13:57:39 +0100

    Update the Start and Stop VNFi process task request status handling.

--- VNF_LCM/Process_Start_VNF/Tasks/.meta_Task_Get_VNF_LCM_Operation.py
+++ VNF_LCM/Process_Start_VNF/Tasks/.meta_Task_Get_VNF_LCM_Operation.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1666015865747</value>
+            <value>1668516938407</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1666015865745</value>
+            <value>1668516938405</value>
         </entry>
         <entry>
             <key>TAG</key>

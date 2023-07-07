793b64599694bd35bc7bceb8389b2213654d2543
bmu <bmu@ubiqube.com>
2023-07-07 15:25:15 +0000

    Retrieve NS Descriptor ID

---
+++ VNF_NS_Packages_Management/Process_Onboard_NSD/Tasks/Task_retrieve_ns_descriptor_id.py
@@ -0,0 +1,41 @@
+import json
+from msa_sdk.variables import Variables
+from msa_sdk.msa_api import MSA_API
+
+from custom.ETSI.NsdSol005 import NsdSol005
+
+REPOSITORY_ROOT_PATH = '/opt/fmc_repository/'
+
+if __name__ == "__main__":
+
+    dev_var = Variables()
+    dev_var.add('ns_package_id', var_type='String')
+    dev_var.add('ns_descriptor_id', var_type='String')
+    context = Variables.task_call(dev_var)
+
+    #Get SOL00X version from context.
+    sol_version = context.get('sol005_version')
+
+    nsdApi = NsdSol005(context["mano_ip"], context["mano_port"], context["mano_base_url"], sol_version)
+        
+    auth_mode = context['auth_mode']
+    if auth_mode == 'oauth2' or auth_mode == 'oauth_v2':
+        nsdApi.set_parameters(context['mano_user'], context['mano_pass'], auth_mode, context['keycloak_server_url'])
+    else:
+        nsdApi.set_parameters(auth_mode, context['mano_user'], context['mano_pass'])
+    
+    #Execute the operation to get the NS Package details.
+    r = nsdApi.nsd_descriptors_get(context["ns_package_id"])
+    
+    r_details = ''
+    status = nsdApi.state
+    if status == 'ENDED':
+        r_details = 'Successful!'
+    else:
+        r_details = str(r.json().get('detail'))
+        
+    #Store NSD id into the context.
+    context["ns_descriptor_id"] = r.json()["nsdId"]
+        
+    ret = MSA_API.process_content(nsdApi.state, f'{r}' + ': ' + r_details, context, True)
+    print(ret)
--- VNF_NS_Packages_Management/Process_Onboard_VNF/Tasks/.meta_Task_retrieve_vnf_descriptor_id.py
+++ VNF_NS_Packages_Management/Process_Onboard_VNF/Tasks/.meta_Task_retrieve_vnf_descriptor_id.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1688637881711</value>
+            <value>1688733225452</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1688637881706</value>
+            <value>1688733225445</value>
         </entry>
         <entry>
             <key>TAG</key>

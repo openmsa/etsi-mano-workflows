6e9b30c54b0ac9282af0a1c4705d50e688706149
Sergio Lebon <sle@ubiqube.com>
2023-09-07 14:33:53 +0000

    Enhance the process to check vnf package and vnfd ids consitency.

--- VNF_LCM/Process_Check_Package_Consistent/Tasks/.meta_Task_is_vnfd_id_consistent.py
+++ VNF_LCM/Process_Check_Package_Consistent/Tasks/.meta_Task_is_vnfd_id_consistent.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1688723867386</value>
+            <value>1694093331188</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1688723867383</value>
+            <value>1694093331184</value>
         </entry>
         <entry>
             <key>TAG</key>
--- VNF_LCM/Process_Check_Package_Consistent/Tasks/Task_is_vnf_package_id_consistent.py
+++ VNF_LCM/Process_Check_Package_Consistent/Tasks/Task_is_vnf_package_id_consistent.py
@@ -15,16 +15,13 @@ if __name__ == "__main__":
     context = Variables.task_call(dev_var)
     
     #SOL005 Package Management
-    vnfPkg = VnfPkgSol005(context["nfvo_mano_ip"], context["nfvo_mano_port"])
+    vnfPkg = VnfPkgSol005(context["nfvo_mano_ip"], context["nfvo_mano_port"], context["nfvo_mano_base_url"])
     
     nfvo_mano_auth_mode = context["nfvo_mano_auth_mode"]
     if nfvo_mano_auth_mode == 'oauth_v2':
-        #Get keycloak server URL.
-        keycloak_server_url = context["nfvo_mano_keycloak_server_url"]
-        
-        vnfPkg.set_parameters(nfvo_mano_user, nfvo_mano_pass, nfvo_mano_auth_mode, keycloak_server_url)
+        vnfPkg.set_parameters(context["nfvo_mano_user"], context["nfvo_mano_pass"], nfvo_mano_auth_mode, context["nfvo_mano_keycloak_server_url"])
     else:
-        vnfPkg.set_parameters(nfvo_mano_user, nfvo_mano_pass)
+        vnfPkg.set_parameters(context["nfvo_mano_user"], context["nfvo_mano_pass"])
         
    #Execute the operation to get the VNF Package details.
     r = vnfPkg.vnf_packages_get_package(context["vnf_pkg_id"])
@@ -37,8 +34,13 @@ if __name__ == "__main__":
         exit()
         
     #Store the VNFD Id for checking in the context.
-    vnfd_id = r.json()["vnfId"]
-    context['vnfd_id_checking'] = vnfd_id
-    
-    ret = MSA_API.process_content(nsLcm.state, f'{r}, {r}', context, True)
-    print(ret)
+    #MSA_API.task_error(json.dumps(r.json()), context, True)
+    if "vnfdId" in r.json():
+        vnfd_id = r.json()["vnfdId"]
+        vnfpkg_id = r.json()["id"]
+        context['vnfd_id_checking'] = vnfd_id
+        ret = MSA_API.process_content(vnfPkg.state, f'{r}, The VNF Package Id still available from NFVO catalog:\nvnf_package_id = ' + vnfpkg_id + '\nvnfd_id = ' + vnfd_id, context, True)
+        print(ret)
+        exit()
+
+    MSA_API.task_error('Failed to found the "vnfdId" attribute when trying to get the VNF Package details from NFVO.')

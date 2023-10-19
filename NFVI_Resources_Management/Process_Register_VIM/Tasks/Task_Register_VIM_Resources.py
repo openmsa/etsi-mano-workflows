8ec18d7d73f242d81a8208bec9fa3d220d64d798
Sergio Lebon <sle@ubiqube.com>
2023-10-19 12:48:02 +0000

    Enhance VIM registration process tasks + add tasks allows to fetch VIM ME config vars.

--- NFVI_Resources_Management/Process_Register_VIM/Tasks/Task_Register_VIM_Resources.py
+++ NFVI_Resources_Management/Process_Register_VIM/Tasks/Task_Register_VIM_Resources.py
@@ -6,71 +6,47 @@ from msa_sdk.device import Device
 from custom.ETSI.NfviVim import NfviVim
 
 if __name__ == "__main__":
-
     dev_var = Variables()
     dev_var.add('vim_device', var_type='Device')
+    dev_var.add("service_instance_name", var_type='String')
     context = Variables.task_call(dev_var)
+    
+    #Lock the workflow instance to be dedicated to the VIM registration management.
+    context["service_instance_assignment"] = 'vim_registration_mgmt'
     
     #Set NFVO access infos.
     nfvo_mano_me_id = context["nfvo_device"][3:]
-    nfvo_mano_ip    = Device(device_id=nfvo_mano_me_id).management_address
-    nfvo_mano_var   = Device(device_id=nfvo_mano_me_id).get_configuration_variable("HTTP_PORT")
-    nfvo_mano_port  = nfvo_mano_var.get("value").strip()
-    nfvo_mano_user  = Device(device_id=nfvo_mano_me_id).login
-    nfvo_mano_pass  = Device(device_id=nfvo_mano_me_id).password
-    
+    nfvo_mano_ip    = context["nfvo_mano_ip"]
+    nfvo_mano_port  = context["nfvo_mano_port"]
+    nfvo_mano_user  = context["nfvo_mano_user"]
+    nfvo_mano_pass  = context["nfvo_mano_pass"]
+    #Get keycloak server URL.
+    nfvo_keycloak_server_url  = context["nfvo_mano_keycloak_server_url"]
     #Get Authentication mode ('basic' or 'oauth2').
-    auth_mode_var   = Device(device_id=nfvo_mano_me_id).get_configuration_variable("AUTH_MODE")
-    auth_mode  = auth_mode_var.get("value").strip()
-    context["auth_mode"] = auth_mode
-    
+    auth_mode  = context["nfvo_mano_auth_mode"]
     #Get NFVO API base url.
-    base_url_var   = Device(device_id=nfvo_mano_me_id).get_configuration_variable("BASE_URL")
-    base_url  = base_url_var.get("value").strip()
-    context["auth_mode"] = base_url
+    base_url  = context["nfvo_mano_base_url"]
     
     #Init NFVI VIM object.
     nfviVim = NfviVim(nfvo_mano_ip, nfvo_mano_port, base_url)
     
-    if auth_mode == 'oauth2' or auth_mode == 'oauth_v2':
-        #Get keycloak server URL.
-        keycloak_url_var   = Device(device_id=nfvo_mano_me_id).get_configuration_variable("SIGNIN_REQ_PATH")
-        keycloak_server_url  = keycloak_url_var.get("value")
-        context["keycloak_server_url"] = keycloak_server_url.strip()
-        
-        nfviVim.set_parameters(nfvo_mano_user, nfvo_mano_pass, auth_mode, context['keycloak_server_url'])
+    if auth_mode == 'oauth_v2':
+        nfviVim.set_parameters(nfvo_mano_user, nfvo_mano_pass, auth_mode, nfvo_keycloak_server_url)
     else:
         nfviVim.set_parameters(nfvo_mano_user, nfvo_mano_pass)
     
     #Get VimCOnnectionInfo, retrieve from openstack Managed Entity configuration vars + ME informations.
     vim_me_id = context["vim_device"][3:]
-    vim__ip    = Device(device_id=vim_me_id).management_address
-    vim_username  = Device(device_id=vim_me_id).login
-    vim_password  = Device(device_id=vim_me_id).password
     
-    vim_type = 'OPENSTACK_V3'
-    
-    http_protocol_var = Device(device_id=vim_me_id).get_configuration_variable("HTTP_PROTOCOL")
-    http_protocol = http_protocol_var.get("value").strip()
-    
-    endpoint = http_protocol  +'://' + vim__ip + ':5000/v3'
-    
-    project_id_var= Device(device_id=vim_me_id).get_configuration_variable("TENANT_ID")
-    project_id = project_id_var.get("value").strip()
-    
-    project_domain_var =  Device(device_id=vim_me_id).get_configuration_variable("PROJECT_DOMAIN_ID")
-    project_domain = project_domain_var.get("value").strip()
-    
-    user_domain_var = Device(device_id=vim_me_id).get_configuration_variable("USER_DOMAIN_ID")
-    user_domain = user_domain_var.get("value").strip()
-    
-    #Get SDN controller (e.g: Juniper contrail) endpoint if exists.
-    sdn_endpoint = ''
-    try:
-        sdn_endpoint_var = Device(device_id=vim_me_id).get_configuration_variable("SDN_CONTROLLER_ENDPOINT")
-        sdn_endpoint = sdn_endpoint_var.get("value").strip()
-    except:
-        pass
+    vim_username = context["vim_username"]
+    vim_password = context["vim_password"]
+    http_protocol = context["vim_http_protocol"]
+    project_id = context["vim_project_id"]
+    project_domain = context["vim_project_domain"]
+    user_domain = context["vim_user_domain"]
+    vim_type = context["vim_type"]
+    endpoint = context["vim_auth_endpoint"]
+    sdn_endpoint = context["vim_sdn_endpoint"]
     
     #InterfaceInfo dict.
     interfaceInfo = {"endpoint": endpoint, "non-strict-ssl": "true"}
@@ -92,7 +68,6 @@ if __name__ == "__main__":
                    "lat": 4.8001016
                    }
                }
-    
     #Add the sdn controller endpoint.
     if sdn_endpoint:
         interfaceInfo['sdn-endpoint'] = sdn_endpoint
@@ -100,6 +75,9 @@ if __name__ == "__main__":
     #Insert InterfaceInfo dict to the main content.
     content.update(interfaceInfo=interfaceInfo)
     
+    context.update(DEBUG_SSLE=content)
+    
+    #MSA_API.task_error('STOP', context, True)
     #Execute the VIM registration to the NFVO
     r = nfviVim.nfvi_vim_register(content)
     
@@ -108,7 +86,7 @@ if __name__ == "__main__":
     if status == 'ENDED':
         r_details = 'Successful!'
     else:
-        if isinstance(r, dict):
+        if isinstance(r.json, dict):
             r_details = str(r.json().get('detail'))
     
     ret = MSA_API.process_content(nfviVim.state, f'{r}' + ': ' + r_details, context, True)

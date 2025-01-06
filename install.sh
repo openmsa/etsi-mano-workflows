0b7cb4bdfb10eaa0c42f7d51014c56fe4fdb2d6e
Olivier Vignaud <ovi@ubiqube.com>
2025-01-06 11:25:04 +0100

    🚑️ Fix install script for circular symlinks.

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ emit_step "ETSI-MANO %b" "$TAG_WF_ETSI_MANO"
 ln -fsn ../etsi-mano-workflows etsi-mano-workflows
 
 popd || exit
-
-ln -sf /opt/fmc_repository/etsi-mano-workflows/src/ /opt/fmc_repository/Process/PythonReference/custom/ETSI
+unlink /opt/fmc_repository/Process/PythonReference/custom/ETSI
+unlink /opt/fmc_repository/etsi-mano-workflows/src/src 2>/dev/null
+ln -sf /opt/fmc_repository/etsi-mano-workflows/src /opt/fmc_repository/Process/PythonReference/custom/ETSI
 
